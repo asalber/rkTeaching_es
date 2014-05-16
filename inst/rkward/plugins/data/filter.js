4b1ad7a7a3904c1d701a532fdd4e332313db3660
Alfredo Sánchez Alberca <asalber@ceu.es>
2014-05-16 13:46:08 +0200

    Fixed labels in new filtered data sets.

--- inst/rkward/plugins/data/filter.js
+++ inst/rkward/plugins/data/filter.js
@@ -1,6 +1,4 @@
 // author: Alfredo Sánchez Alberca (asalber@ceu.es)
-
-// globals
 var data, condition, filtervariables, variables;
 
 function preprocess(){
@@ -12,16 +10,21 @@ function calculate () {
 	var newdata = getString("save");
 	condition = getString("condition");
 	filtervariables = getBoolean ("variables_frame.checked");
-	echo ('.GlobalEnv$' + newdata + ' <- subset(' + data + ', subset=' + condition);
+	// Create a new dataset with the filtered data
+	echo('.GlobalEnv$' + newdata + ' <- subset(' + data + ', subset=' + condition);
 	if (filtervariables){
 		variables = getList("variables.shortname").join (', ');
 		if (variables != '') echo (', select=c(' + variables +')');
 	}
-	echo (')\n');	
+	echo(')\n');
+	// Copy also the labels of original data set
+	echo('for(i in 1:length(names(' + newdata + '))){\n');
+    echo('\t attr(.GlobalEnv$' + newdata + '[[names(' + newdata + ')[i]]],".rk.meta") = attr(' + data + '[[names(' + newdata + ')[i]]],".rk.meta")\n');
+    echo('}\n');
 }
 
 function printout () {
-	echo ("rk.header ('Filtrado de datos', parameters=list('Conjunto de datos' = '" + data + "'"); 
+	echo("rk.header ('Filtrado de datos', parameters=list('Conjunto de datos' = '" + data + "'"); 
 	if (condition!='') echo(", 'Condici&oacute;n de selecci&oacute;n' = '" + condition + "'");
 	if (filtervariables && variables!='') echo(", 'Variables seleccionadas' = '" + variables + "'");
 	echo('))\n');
