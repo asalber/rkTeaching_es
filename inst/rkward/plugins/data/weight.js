8cabf91143f9953116ef2c566d96bcb11d208284
Alfredo Sánchez Alberca <asalber@ceu.es>
2014-05-16 15:28:31 +0200

    Added labels to variables of weighted dataframe.

--- inst/rkward/plugins/data/weight.js
+++ inst/rkward/plugins/data/weight.js
@@ -13,6 +13,12 @@ function calculate () {
 	newdata = getString("save");
 	freq = getString("freq.shortname");
 	echo ('.GlobalEnv$' + newdata + ' <- weightDataFrame(' + data + ', "' + freq + '")\n');	
+	// Copy also the labels of original data set
+	echo('for(i in 1:length(names(' + newdata + '))){\n');
+	echo('\t if (names(' + newdata + ')[i]!= "' + freq + '"){\n');
+    echo('\t attr(.GlobalEnv$' + newdata + '[[names(' + newdata + ')[i]]],".rk.meta") = attr(' + data + '[[names(' + newdata + ')[i]]],".rk.meta")\n');
+    echo('\t }\n');
+    echo('}\n');
 }
 
 function printout () {
