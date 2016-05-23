76cb927fe38b5e2a6304cda7068e807ec496fb96
Alfredo Sánchez Alberca <asalber@gmail.com>
2016-05-23 10:27:11 +0200

    Añadido artículo matemáticas everywhere.

--- inst/rkward/plugins/simulations/small_numbers_law.js
+++ inst/rkward/plugins/simulations/small_numbers_law.js
@@ -2,7 +2,6 @@
 
 function preprocess(){
 	// add requirements etc. here
-	echo('require(TeachingExtras)\n');
 }
 
 function printout () {
@@ -29,12 +28,12 @@ function doPrintout (full) {
 	echo ('p <- ' +  p + '\n');
 	echo ('mu <- p*n\n');
 	echo ('sd <- sqrt(n*p*(1-p))\n');
-	echo ('plot( seq(0,n), dpois( seq(0,n), mu ), type="h", xlim=c(-1,n+1), xlab="x", ylab="Probability", ylim=range(0,dpois( seq(0,n), mu), dbinom(seq(0,n),n,p)))\n');
+	echo ('plot( seq(0,n), dpois( seq(0,n), mu ), type="h", xlim=c(-1,n+1), xlab="x", ylab="Probabilidad", ylim=range(0,dpois( seq(0,n), mu), dbinom(seq(0,n),n,p)))\n');
 	echo ('points( seq(0,n), dpois( seq(0,n), mu ), pch=16, col="blue")\n');
 	echo ('points( seq(0,n), dbinom( seq(0,n), n, p), type="h")\n');
 	echo ('abline(h=0)\n');
 	echo ('points( seq(0,n), dbinom( seq(0,n), n, p), pch=18, col="red" )\n');
-	echo ('title( paste("Mean =",round(mu,3),"Std. Dev. =",round(sd,3)))\n');
+	echo ('title( paste("Media =",round(mu,3),"Desv.Tip. =",round(sd,3)))\n');
 	echo ('legend("topright", c("Binomial", "Poisson"), col = c("red","blue"), pch = c(18,16))\n');
 
 	echo ('})\n');
