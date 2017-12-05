5f448cd1ba3c5437d6c02fe166e5ac39e2841118
Alfredo Sánchez Alberca <asalber@gmail.com>
2017-12-05 11:57:32 +0100

    Eliminada dependencia del paquete prob.

--- inst/rkward/plugins/data/variable_calculation.js
+++ inst/rkward/plugins/data/variable_calculation.js
@@ -1,23 +1,24 @@
 // author: Alfredo Sánchez Alberca (asalber@ceu.es)
 
 // globals
-var data, variable, expresion;
+var data,
+    variable,
+    expresion;
 
-function preprocess(){
-	// add requirements etc. here
+function preprocess() {
+    // add requirements etc. here
 }
 
-function calculate () {
-	variable = getString("save");
-	data = getString("dataframe");
-	expresion = getString("expression");
-	// Create a new variable from a formula
-	echo ('.GlobalEnv$' + variable + ' <- with(' + data + ', ' + expresion + ')\n');	
-	// Clean the label of the new variable
+function calculate() {
+    variable = getString("save");
+    data = getString("dataframe");
+    expresion = getString("expression");
+    // Create a new variable from a formula
+    echo('.GlobalEnv$' + variable + ' <- with(' + data + ', ' + expresion + ')\n');
+    // Clean the label of the new variable
     echo('\t attr(.GlobalEnv$' + variable + ',".rk.meta") = NULL\n');
 }
 
-function printout () {
-	echo ('rk.header ("C&aacute;lculo de variable", parameters=list("Conjunto de datos" = "' + data + '", "Nueva variable"= rk.get.description(' + variable + '), "Expresi&oacute;n" = "' + expresion + '"))\n');
+function printout() {
+    echo('rk.header ("C&aacute;lculo de variable", parameters=list("Conjunto de datos" = "' + data + '", "Nueva variable"= rk.get.description(' + variable + '), "Expresi&oacute;n" = "' + expresion + '"))\n');
 }
-
